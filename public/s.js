f4214517865ee074ad3a575d449fa809ffa2b1cd
George Edwards <ge@speedwelshpool.com>
2021-02-22 23:36:00 +0000

    disco dance

--- public/s.js
+++ public/s.js
@@ -1,13 +1,19 @@
 const player = document.getElementById("player");
 const bigRedButton = document.getElementById("button");
+const header = document.getElementById("header");
 const subHeader = document.getElementById("subHeader");
+const dancer = document.getElementById("dancer");
 
 bigRedButton.addEventListener("click", async () => {
   if (player.paused) {
     await player.play();
-    subHeader.innerHTML = "Leave your cares behind... press to pause";
+    header.classList.add("hide");
+    subHeader.classList.add("hide");
+    dancer.classList.remove("hide");
   } else {
     await player.pause();
-    subHeader.innerHTML = "Press to play...";
+    header.classList.remove("hide");
+    subHeader.classList.remove("hide");
+    dancer.classList.add("hide");
   }
 });
